d05e1aa0aabff43dc310790998bac490f43f8440
Kuitar5 <levi.kuitar@gmail.com>
2023-01-14 17:47:01 +0100

    The .gd file
    The “.gd” file, “myfirstlevel.gd” in my situation, will create the definition for our DGO file, aka our full level file. In short, we’ll have to put everything that needs to be included inside of our level here.
    For now, we’ll just have to change a few names as such:

--- Examples/Custom Levels Guide/data/custom_levels/my-first-level/myfirstlevel.gd
+++ Examples/Custom Levels Guide/data/custom_levels/my-first-level/myfirstlevel.gd
@@ -1,8 +1,8 @@
-;; DGO definition file for Awful Village level
+;; DGO definition file for My first Level
 ;; We use the convention of having a longer DGO name for levels without precomputed visibility.
 
 ;; the actual file name still needs to be 8.3
-("TSZ.DGO"
-  ("static-screen.o" "static-screen")
-  ("test-zone.go" "test-zone")
+("MFL.DGO"                                      ;; 3 Letter name for the final level file
+  ("static-screen.o" "static-screen")           
+  ("my-first-level.go" "my-first-level")        ;; the .go of our level which will be created by the compiler
   )
